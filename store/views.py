aba4bcdf32b0f37288fcbcaefe68e73a3e16b26d
Pedro Molina <pedro.molinanoa@gmail.com>
2024-01-04 15:07:52 -0500

    Django CRUD

--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializer import StoreSerializer
+from .models import Producto
 # Create your views here.
+
+
+class StoreView(viewsets.ModelViewSet):
+    serializer_class = StoreSerializer
+    queryset = Producto.objects.all()
