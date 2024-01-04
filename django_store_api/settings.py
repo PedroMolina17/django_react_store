7a5c6a4f86e50ad970e4cd21568fdfbf6f3ac8c7
Pedro Molina <pedro.molinanoa@gmail.com>
2024-01-04 15:28:30 -0500

    Add documentation

--- django_store_api/settings.py
+++ django_store_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'coreapi',
     'store'
 ]
 
@@ -131,3 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
 
 ]
+
+REST_FRAMEWORK = {
+    ...: ...,
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import StoreView
-
+from rest_framework.documentation import include_docs_urls
 router = routers.DefaultRouter()
 router.register(r'store', StoreView, 'store')
 
 urlpatterns = [
-    path("api/v1/", include(router.urls))
+    path("api/v1/", include(router.urls)),
+    path('docs/', include_docs_urls(title="Store documentation"))
 ]
