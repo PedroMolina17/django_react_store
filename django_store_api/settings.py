f4aca256af5ebe3c8dd98b477717b68ab4267dd8
Pedro Molina <pedro.molinanoa@gmail.com>
2024-01-04 04:47:20 -0500

    SQLite Product

--- django_store_api/settings.py
+++ django_store_api/settings.py
@@ -129,8 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # cors authorization
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
+
 ]
--- store/models.py
+++ store/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True)
+    en_stock = models.IntegerField(default=0)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(
+        upload_to='producto_imagenes/', blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
