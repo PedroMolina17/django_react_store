1e429761e369269f62b0f64aa9fe3bee161c0888
pedromolina17 <pedro.molinanoa@gmail.com>
2024-01-13 20:44:24 -0500

    def by categoria

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from .serializer import StoreSerializer, GamesSerializer, CategoriaSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Producto
 from .models import Games
 from .models import Categoria
@@ -9,6 +11,17 @@ from .models import Categoria
 class StoreView(viewsets.ModelViewSet):
     serializer_class = StoreSerializer
     queryset = Producto.objects.all()
+
+    @action(detail=False, methods=['GET'])
+    def get_by_categoria(self, request):
+        categoria_id = request.GET.get('categoria_id')
+        if categoria_id:
+            productos = Producto.objects.filter(categoria=categoria_id)
+        else:
+            productos = Producto.objects.all()
+
+        serializer = StoreSerializer(productos, many=True)
+        return Response(serializer.data)
 
 
 class GamesView(viewsets.ModelViewSet):
